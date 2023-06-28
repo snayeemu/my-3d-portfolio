9c634d40a5836365404e4b0450f7e6c22bc1d526
snayeemu <snayeemu2@gmail.com>
2023-06-28 17:18:12 +0600

    light bug fixed

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -9,14 +9,13 @@ import {
 } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
-import { AmbientLight } from "three";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <AmbientLight intensity={0.25} />
+      <ambientLight intensity={0.25} />
       <directionalLight position={(0, 0, 0.05)} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
