ed689856de39bae87e5c2b0d6a4a517b2a78d12c
snayeemu <snayeemu2@gmail.com>
2023-06-28 17:13:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter } from "react-router-dom";
+import About from "./components/About";
+import Stars from "./components/canvas/Stars";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Feedbacks from "./components/Feedbacks";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar></Navbar>
+          <Hero></Hero>
+        </div>
+        <About></About>
+        <Experience></Experience>
+        <Tech></Tech>
+        <Works></Works>
+        <Feedbacks></Feedbacks>
+        <div className="relative z-0">
+          <Contact></Contact>
+          <Stars></Stars>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/canvas/Ball.jsx
@@ -0,0 +1,47 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+import { AmbientLight } from "three";
+
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <AmbientLight intensity={0.25} />
+      <directionalLight position={(0, 0, 0.05)} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color={"#fff8eb"}
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal position={[0, 0, 1]} map={decal} />
+      </mesh>
+    </Float>
+  );
+};
+
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
